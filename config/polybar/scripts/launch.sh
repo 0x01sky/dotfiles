abf1fc59cfb923b41f20fe4d5c5bfdc88ba53afc
sky <sky@git.org>
2026-06-25 13:23:09 +0100

    Updating dotfiles

--- config/polybar/scripts/launch.sh
+++ config/polybar/scripts/launch.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Other monitors
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
+while pgrep -x polybar >/dev/null; do
+    sleep 0.2
+done
+
+sleep 1.5
+
+if type xrandr >/dev/null 2>&1; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mybar &
+    done
 else
-  polybar --reload mybar &
+    polybar --reload mybar &
 fi
